ca355e5b036b290b20a9ab039c17b5b3120bc224
Andrew HoChoy <a.hochoy@gmail.com>
2026-03-11 12:51:34 -0700

    Add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })]
 });
